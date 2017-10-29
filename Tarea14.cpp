0f8d75ffcf17551aa65aa270b3c770a0a032c5f5
vanyjc <31527650+vanyjc@users.noreply.github.com>
2017-10-28 19:46:30 -0500

    Add files via upload

---
+++ Tarea14.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#define MAX 5
+
+int main(){
+	
+	int edad=0;
+	const float pi=3.1416;
+	
+	printf("Cual es tu edad?: ");
+	scanf("%d",&edad);
+	
+	printf("Tu edad es=%d \n",edad);
+	printf("Maximo=%d",MAX);
+	printf("\nValor de pi=%f",pi);
+	
+	return 0;
+	
+}
